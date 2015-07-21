90839c92111f483227dde602ff34440877a98f4f
Nuullll <vfirst218@gmail.com>
2015-07-21 22:48:32 +0800

    Sound single note

---
+++ src/soundnote.m
@@ -0,0 +1,9 @@
+function soundnote(key,nnote,flag,tn,fs,tp)
+
+% sound note; note lasts for time tn, pause for time tp
+t = 0:1/fs:tn;
+sound(sin(2*pi*freqmap(key,nnote,flag)*t),fs);
+pause(tp);
+
+end
+
