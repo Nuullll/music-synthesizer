9b65e52fbc33ba23db29e35339606c5374cee121
Nuullll <vfirst218@gmail.com>
2015-07-21 23:01:50 +0800

    Fix pause time calculate error

--- src/soundnote.m
+++ src/soundnote.m
@@ -3,7 +3,7 @@ function soundnote(key,nnote,flag,tn,fs,tp)
 % sound note; note lasts for time tn, pause for time tp
 t = 0:1/fs:tn;
 sound(sin(2*pi*freqmap(key,nnote,flag)*t),fs);
-pause(tp);
+pause(tn+tp);
 
 end
 
