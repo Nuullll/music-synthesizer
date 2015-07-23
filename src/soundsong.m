858898e5c6c9c23d07c8b151a0453ea66410da2e
Nuullll <vfirst218@gmail.com>
2015-07-23 23:44:02 +0800

    Add adsr interface for soundsong

--- src/soundsong.m
+++ src/soundsong.m
@@ -1,7 +1,7 @@
 % src/soundsong.m
 
-function wav = soundsong(BPM,key,flag,song,fs)
-% soundsong(BPM,key,song,fs)
+function wav = soundsong(BPM,key,flag,song,fs,ADSR)
+% soundsong(BPM,key,song,fs,ADSR)
 % 输入:
 %   <float> BPM: beats per minute
 %   <char> key: 歌曲的调
@@ -9,16 +9,17 @@ function wav = soundsong(BPM,key,flag,song,fs)
 %   <n-by-2 matrix> song: 乐谱, 第一列为音符唱名, 第二列为各音符持续拍数
 %       休止符用-inf表示
 %   <float> fs: 采样频率
+%   <1-by-4 matrix> ADSR: adsr包络控制
 % 返回值:
 %   <row vector> wav: 合成的歌曲
 
 tpb = 60/BPM;   % time per beat (seconds)
 wav = [];       % intialize wav
 
-attack = 0.1;
-decay = 0.7;
-sustain = 0;
-release = 0.2;
+attack = ADSR(1);
+decay = ADSR(2);
+sustain = ADSR(3);
+release = ADSR(4);
 
 for i = 1:size(song,1)
     t = 0:1/fs:(tpb*song(i,2));     % time sequence
