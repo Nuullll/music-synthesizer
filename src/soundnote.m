3ba3bfd8efa4bc78e583f6644e29ea9be29a4c46
Nuullll <vfirst218@gmail.com>
2015-07-23 18:37:41 +0800

    Delete debug .m

--- src/soundnote.m
+++
@@ -1,9 +0,0 @@
-function soundnote(key,nnote,flag,tn,fs,tp)
-
-% sound note; note lasts for time tn, pause for time tp
-t = 0:1/fs:tn;
-sound(sin(2*pi*freqmap(key,nnote,flag)*t),fs);
-pause(tn+tp);
-
-end
-
