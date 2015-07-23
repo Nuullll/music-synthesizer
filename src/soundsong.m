12fe3f4f21a9deb1e731237fb741605fa345f342
Nuullll <vfirst218@gmail.com>
2015-07-23 18:50:47 +0800

    Change presentation of pause notation

--- src/soundsong.m
+++ src/soundsong.m
@@ -7,7 +7,7 @@ function wav = soundsong(BPM,key,flag,song,fs)
 %   <char> key: 歌曲的调
 %   <int> flag: -1表示降半个音阶(?), 1表示升半个音阶(?)
 %   <n-by-2 matrix> song: 乐谱, 第一列为音符唱名, 第二列为各音符持续拍数
-%       休止符用字符'-'表示
+%       休止符用-inf表示
 %   <float> fs: 采样频率
 % 返回值:
 %   <row vector> wav: 合成的歌曲
@@ -16,7 +16,7 @@ tpb = 60/BPM;   % time per beat (seconds)
 wav = [];       % intialize wav
 for i = 1:size(song,1)
     t = 0:1/fs:(tpb*song(i,2));     % time sequence
-    if song(i,1) == '-'
+    if song(i,1) == -inf
         wav = [wav, zeros(1,length(t))];
     else
         wav = [wav, sin(2*pi*freqmap(key,song(i,1),flag)*t)];
